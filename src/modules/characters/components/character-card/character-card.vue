66efeb99cc6afea702dbcae418c8021c6c634693
ToasT <16761924+toastological@users.noreply.github.com>
2025-08-18 22:48:59 -0700

    make cards wrapped in a tag

--- src/modules/characters/components/character-card/character-card.vue
+++ src/modules/characters/components/character-card/character-card.vue
@@ -1,51 +1,57 @@
 <style lang="scss" scoped src="./character-card.scss"></style>
 
 <template>
-    <Card
-        class="cat-card"
-        :class="[ cat.type, { hoverable }]"
-        @click="onAerocatClicked"
-        ref="characterCard">
-        <template #header>
-            <div
-                class="image-container"
-                ref="imageContainer">
-                <Skeleton
-                    v-if="loading"
-                    class="character-card-skeleton">
-                </Skeleton>
-                <img
-                    class="cat-card-img"
-                    alt="aerocat profile image"
-                    :data-src="cat.galleryUrls[0]"
-                    :data-loading="loading"
-                    @load="onImageLoad"/>
-            </div>
-        </template>
-        <template #title>
-            <div class="cat-model">
-                {{ cat?.model }}
-            </div>
-        </template>
-        <template #subtitle>
-            <div class="cat-name">
-                {{ cat?.name }}
-            </div>
-        </template>
-        <template #footer>
-            <div
-                v-if="cat?.creator"
-                class="creator"
-                ref="creatorContainer">
-                <img 
-                    v-if="cat?.creator?.profileUrl"
-                    class="creator-img" 
-                    alt="creator profile image" 
-                    :data-src="cat?.creator?.profileUrl"/>
-                <span class="creator-name"> {{ cat?.creator?.name }} </span>
-            </div>
-        </template>
-    </Card>
+    <div class="character-card-container">
+        <router-link
+            class="cat-wrapper"
+            :to="{ name: RouteNames.Character, params: { id: cat.id || 0 }}">
+            <Card
+                class="cat-card"
+                :class="[ cat.type, { hoverable }]"
+                @click="onAerocatClicked"
+                ref="characterCard">
+                <template #header>
+                    <div
+                        class="image-container"
+                        ref="imageContainer">
+                        <Skeleton
+                            v-if="loading"
+                            class="character-card-skeleton">
+                        </Skeleton>
+                        <img
+                            class="cat-card-img"
+                            alt="aerocat profile image"
+                            :data-src="cat.galleryUrls[0]"
+                            :data-loading="loading"
+                            @load="onImageLoad"/>
+                    </div>
+                </template>
+                <template #title>
+                    <div class="cat-model">
+                        {{ cat?.model }}
+                    </div>
+                </template>
+                <template #subtitle>
+                    <div class="cat-name">
+                        {{ cat?.name }}
+                    </div>
+                </template>
+                <template #footer>
+                    <div
+                        v-if="cat?.creator"
+                        class="creator"
+                        ref="creatorContainer">
+                        <img 
+                            v-if="cat?.creator?.profileUrl"
+                            class="creator-img" 
+                            alt="creator profile image" 
+                            :data-src="cat?.creator?.profileUrl"/>
+                        <span class="creator-name"> {{ cat?.creator?.name }} </span>
+                    </div>
+                </template>
+            </Card>
+        </router-link>
+    </div>
 </template>
 
 <script setup lang="ts">
