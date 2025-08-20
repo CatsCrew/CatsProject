8d70c84220461edc8e688fc731a2293b32534ada
ToasT <16761924+toastological@users.noreply.github.com>
2025-08-19 18:16:29 -0700

    check if hoverable since placeholder card doesnt need to defer or anything special

--- src/modules/characters/components/character-card/character-card.vue
+++ src/modules/characters/components/character-card/character-card.vue
@@ -2,28 +2,68 @@
 
 <template>
     <div class="character-card-container">
-        <router-link
-            class="cat-wrapper"
-            :to="{ name: RouteNames.Character, params: { id: cat.id || 0 }}">
+        <template v-if="hoverable">
+            <router-link
+                class="cat-wrapper"
+                :to="{ name: RouteNames.Character, params: { id: cat.id || 0 }}">
+                <Card
+                    class="cat-card"
+                    :class="[ cat.type, { hoverable }]"
+                    @click="onAerocatClicked"
+                    ref="characterCard">
+                    <template #header>
+                        <div
+                            class="image-container"
+                            ref="imageContainer">
+                            <Skeleton
+                                v-if="loading"
+                                class="character-card-skeleton">
+                            </Skeleton>
+                            <img
+                                class="cat-card-img"
+                                alt="aerocat profile image"
+                                :data-src="cat.galleryUrls[0]"
+                                :data-loading="loading"
+                                @load="onImageLoad"/>
+                        </div>
+                    </template>
+                    <template #title>
+                        <div class="cat-model">
+                            {{ cat?.model }}
+                        </div>
+                    </template>
+                    <template #subtitle>
+                        <div class="cat-name">
+                            {{ cat?.name }}
+                        </div>
+                    </template>
+                    <template #footer>
+                        <div
+                            v-if="cat?.creator"
+                            class="creator"
+                            ref="creatorContainer">
+                            <img 
+                                v-if="cat?.creator?.profileUrl"
+                                class="creator-img" 
+                                alt="creator profile image" 
+                                :data-src="cat?.creator?.profileUrl"/>
+                            <span class="creator-name"> {{ cat?.creator?.name }} </span>
+                        </div>
+                    </template>
+                </Card>
+            </router-link>
+        </template>
+        <template v-else>
             <Card
-                class="cat-card"
-                :class="[ cat.type, { hoverable }]"
-                @click="onAerocatClicked"
-                ref="characterCard">
+                class="cat-card">
                 <template #header>
                     <div
                         class="image-container"
                         ref="imageContainer">
-                        <Skeleton
-                            v-if="loading"
-                            class="character-card-skeleton">
-                        </Skeleton>
                         <img
                             class="cat-card-img"
                             alt="aerocat profile image"
-                            :data-src="cat.galleryUrls[0]"
-                            :data-loading="loading"
-                            @load="onImageLoad"/>
+                            :src="cat.galleryUrls[0]"/>
                     </div>
                 </template>
                 <template #title>
@@ -31,26 +71,8 @@
                         {{ cat?.model }}
                     </div>
                 </template>
-                <template #subtitle>
-                    <div class="cat-name">
-                        {{ cat?.name }}
-                    </div>
-                </template>
-                <template #footer>
-                    <div
-                        v-if="cat?.creator"
-                        class="creator"
-                        ref="creatorContainer">
-                        <img 
-                            v-if="cat?.creator?.profileUrl"
-                            class="creator-img" 
-                            alt="creator profile image" 
-                            :data-src="cat?.creator?.profileUrl"/>
-                        <span class="creator-name"> {{ cat?.creator?.name }} </span>
-                    </div>
-                </template>
             </Card>
-        </router-link>
+        </template>
     </div>
 </template>
 
@@ -82,10 +104,9 @@ function onImageLoad() {
 }
 
 onMounted(() => {
-    DeferHelper.defer(characterCard.$el);
-});
-
-onUpdated(() => {
-    DeferHelper.defer(characterCard.$el);
+    if (characterCard) {
+        DeferHelper.defer(characterCard.$el);
+    }
+    
 });
 </script>
