0c65d89b2fb84799d18e38489a4f1c3d3ecb2179
ToasT <16761924+toastological@users.noreply.github.com>
2025-08-29 21:59:54 -0700

    remove unused json properties

--- src/models/lore-document.model.ts
+++ src/models/lore-document.model.ts
@@ -3,7 +3,5 @@ import { Translation } from "./translation.model";
 export interface LoreDocument {
     title?: string;
     description?: string;
-    category?: string;
-    lastUpdated?: string;
     languages?: Translation[];
 }
