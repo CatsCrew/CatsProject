02adc56eb971a04895a219970db0bd915311c412
ToasT <16761924+toastological@users.noreply.github.com>
2025-09-08 19:46:34 -0700

    attempt to tell people to refresh when deployments happen

--- src/app.vue
+++ src/app.vue
@@ -5,6 +5,14 @@
   <div class="terminal-overlay"></div>
   <main class="main-content">
     <RouterView></RouterView>
+    <div
+      v-if="showNewVersionOverlay"
+      class="new-version-overlay">
+      <p class="new-version-text">
+        A new update is available. Please refresh to get the latest version.
+      </p>
+      <Button @click="reloadApp">Refresh Now</Button>
+    </div>
   </main>
   <Footer></Footer>
   <Toast group="desktop"/>
@@ -14,11 +22,37 @@
 <script setup lang="ts">
 import Header from './components/header/header.vue';
 import Footer from './components/footer/footer.vue';
+import Button from 'primevue/button';
 import { useCatsStore } from './store';
 import Toast from 'primevue/toast';
+import { useRouter } from 'vue-router';
+
+const router$ = useRouter();
 
 const cats$ = useCatsStore();
 cats$.initialize();
+
+let showNewVersionOverlay = $ref(false);
+
+router$.onError((error) => {
+  const isChunkLoadError =
+    error.name === 'ChunkLoadError' ||
+    (error.message &&
+      (error.message.includes('Failed to fetch dynamically imported module') ||
+        error.message.includes(
+          "'text/html' is not a valid JavaScript MIME type",
+        ) ||
+        // For Webpack:
+        error.message.includes('Loading chunk failed')));
+
+  if (isChunkLoadError) {
+    showNewVersionOverlay = true;
+  }
+});
+
+function reloadApp() {
+  window.location.reload();
+}
 </script>
 
 
